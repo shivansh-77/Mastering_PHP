e0833acd1c2bb429725b151e5a189b4ecc0d605d
shivansh-77 <shivanshbajpai.29341@gmail.com>
2025-02-18 20:23:36 +0530

    Initial commit

---
+++ php_testing/php_testing.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP testing</title>
+</head>
+<body>
+	<h1>Hello this is PHP where we will see if the PHP and html working together</h1>
+	<h2>hello let's test it <?php echo "Yo ! this is written in the PHP tag"; ?></h2>
+	<h3>so we will the check rand function here <?php echo rand(1,6); ?></h3>
+</body>
+</html>
