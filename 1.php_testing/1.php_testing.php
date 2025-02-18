e31ab23f1f8237b3544015b3327922dc5822ae39
shivansh-77 <shivanshbajpai.29341@gmail.com>
2025-02-18 21:59:35 +0530

    Tested some of the basics

--- 1.php_testing/1.php_testing.php
+++ 1.php_testing/1.php_testing.php
@@ -8,6 +8,6 @@
 <body>
 	<h1>Hello this is PHP where we will see if the PHP and html working together</h1>
 	<h2>hello let's test it <?php echo "Yo ! this is written in the PHP tag"; ?></h2>
-	<h3>so we will the check rand function here <?php echo rand(1,6); ?></h3>
+	<?php echo "<h3>so we will the check rand function here </h3>" . rand(1,6); ?>
 </body>
 </html>
---
+++ 1.php_testing/2.include_file.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>hii</title>
+</head>
+<body>
+	<h1><?php echo "heyyyyyyyy"; ?></h1>
+</body>
+</html>
